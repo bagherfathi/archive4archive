39789b9319dda2d7548b25d33005945da8cea771
luoxngood <luoxngood@c46cfd5c-866d-11de-9934-f1fc26e2a4cc>
2009-09-24 16:05:34 +0000

    up

--- trunk/src/java/web/com/renhenet/web/structure/Test.java
+++ trunk/src/java/web/com/renhenet/web/structure/Test.java
@@ -10,16 +10,20 @@ public class Test {
 		// String str = "#elseif($strIndex=='a2')\n#set($values=$!bizObj.a2)";
 		// String str = "#elseif($strIndex=='a2')\n<td
 		// class='tablelistbg'>&nbsp;#if($!file.a2!='null')$!file.a2#end</td>";
-//		String str ="<property name=\"a2\" type=\"java.lang.String\">\n<column name=\"A2\" length=\"120\" />\n</property>";
-		String str ="private String a2;";
-//		String str1 = "";
-		
-		
+		// String str ="<property name=\"a2\"
+		// type=\"java.lang.String\">\n<column
+		// name=\"A2\" length=\"120\" />\n</property>";
+		String str = "#elseif($strIndex=='a2')\n	"
+				+ "#if($!biz.isSelfMotion==1&&($!bizObj.a2!='null'||$!bizObj.a2!=''))\n		"
+				+ "#set($values=$vMUtils.stringToInt($!bizObj.a2)+1)\n	"
+				+ "#else\n		" + "#set($values=$!bizObj.a2)\n	"
+				+ "#end";
+		// String str1 = "";
 
 		for (int i = 2; i <= 100; i++) {
 			String str1 = "";
 			str1 = str.replaceAll("a2", "a" + i);
-//			str1 = str1.replaceAll("A2", "A" + i);
+			// str1 = str1.replaceAll("A2", "A" + i);
 			System.out.println(str1);
 		}
 
