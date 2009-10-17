152792b8c281d4a68ebd1c97aa1da8c142ef5997
tingwei.xiang <tingwei.xiang@c46cfd5c-866d-11de-9934-f1fc26e2a4cc>
2009-10-17 13:27:21 +0000

    m

--- trunk/docs/oracleview/views.sh
+++ trunk/docs/oracleview/views.sh
@@ -1,7 +1,32 @@
 #!/bin/bash
 #
-#       svaggu 09/28/05 -  Creation
+#       Tingwei 10/14/09-  Creation
 #
+agv="$1"
+One=false
+if [ "$agv" = "" ];then
+	agv="--help"
+fi
+
+if [ "$agv" = "--help" ];then
+	echo
+	echo	"**********************    Help    ***********************"
+	echo
+	echo	"	-c [view Number]:" 
+	echo	"		To create a view by view id"
+	echo	"	-a:"
+	echo 	"		To create all views"
+	exit;
+fi
+
+if [ "$agv" = "-c" ];then
+	One=true;
+	if [ "$2" = "" ];then
+		echo "Error:Need ViewId after -c"
+		exit;
+	fi
+	ViewId="$2";
+fi
 
 ORACLE_HOME=/usr/lib/oracle/xe/app/oracle/product/10.2.0/server
 export ORACLE_HOME
@@ -12,92 +37,102 @@ export NLS_LANG
 PATH=$ORACLE_HOME/bin:$PATH
 export PATH
 
+echo
+echo "Start to create or replace views ...."
+sleep 1;
+
 #************************  get infosort ***************************************
+#select 911911911,INFO_SORT_ID from STRUCTURE group by INFO_SORT_ID;
 Result=`/usr/lib/oracle/xe/app/oracle/product/10.2.0/server/bin/sqlplus -s archive/archive <<EOF
-select 911911911,INFO_SORT_ID from STRUCTURE group by INFO_SORT_ID;
+select 911911911,a.INFO_SORT_ID from STRUCTURE a,info_sort b where a.info_sort_id=b.id group by a.INFO_SORT_ID;
 exit
 EOF`
 
 Result=`echo "${Result}"|grep 911911911|grep -i -v INFO|awk '{print $2}'`
 StructureIdList=${Result}
 
-#************************ get structure ***************************************
+#******************************************* Get structure *********************************************************
 for i in ${StructureIdList};do
-echo index is:${i}
-echo 
-Result=`/usr/lib/oracle/xe/app/oracle/product/10.2.0/server/bin/sqlplus -s archive/archive <<EOF
-set heading off;
-set pagesize 0;
-set linesize 10240;
-select ZN_NAME from STRUCTURE where info_sort_id=${i} ;
-exit
-EOF`
-Result=`echo  "${Result}"|grep -v 已|grep -v selected`
-Result=`echo  ${Result}|sed 's/ /,/g'|sed 's/,/,F/g'`
-FieldList=F${Result}
-echo ${FieldList}
+	
+	if [ ${One} = "true" ];then
+		if [ "${i}" = "${ViewId}" ];then
+			echo "Match";	
+		else
+			continue;
+		fi
+	fi
 
-Result=`/usr/lib/oracle/xe/app/oracle/product/10.2.0/server/bin/sqlplus -s archive/archive <<EOF
-set heading off;
-set pagesize 0;
-set linesize 10240;
-select SERIAL_NUMBER from STRUCTURE where info_sort_id=${i} ;
-exit
-EOF`
-Result=`echo  "${Result}"|grep -v 已|grep -v selected`
-Result=`echo  ${Result}|sed 's/ /,/g'`
-ValueList=${Result}
-echo ${ValueList}
+	echo ViewID:${i}
+	echo 
 
-Result=`/usr/lib/oracle/xe/app/oracle/product/10.2.0/server/bin/sqlplus -s archive/archive <<EOF
-set heading off;
-set pagesize 0;
-set linesize 10240;
-select name from info_sort t start with id= ${i} connect by id = prior parent_id;
-exit
-EOF`
-Result=`echo  "${Result}"|grep -v 已|grep -v selected`
-ViewName=`echo ${Result}|awk '{for(i=0;i<=NF-1;i++)printf("%s ",$(NF-i));printf("\n");}'|sed 's/ /_/g'|sed 's/\_$//g'`
-echo ${ViewName}
+	#******************************************* Get Name List *********************************************************
+	Result=`/usr/lib/oracle/xe/app/oracle/product/10.2.0/server/bin/sqlplus -s archive/archive <<EOF
+	set heading off;
+	set pagesize 0;
+	set linesize 10240;
+	select ZN_NAME from STRUCTURE where info_sort_id=${i} ;
+	exit
+	EOF`
+	Result=`echo  "${Result}"|grep -v 已|grep -v selected`
+	Result=`echo ${Result}`
+	number=1
+	for k in ${Result};do
+		if [ ${number} = 1 ];then
+			FieldList=F${number}${k};
+			
+		else	
+			FieldList=${FieldList},F${number}${k};
+		fi
+		number=`expr ${number} + 1`;
+	done
+	#Result=`echo  ${Result}|sed 's/ /,F/g'`
+	FieldList="INFO_SORT_ID,STATUS,PAR_INFO_SORT_ID,TYPE,TITLE,${FieldList}"
+	#echo ${FieldList}
 
-#Create="Create Or Replace View ${ViewName} ( ${FieldList} ) As Select ${ValueList} From files;"
-Create="Create Or Replace View View${i} ( ${FieldList} ) As Select ${ValueList} From files;"
-echo ${Create}
+	#**********************************************************  Get Value List **************************************************************************
+	Result=`/usr/lib/oracle/xe/app/oracle/product/10.2.0/server/bin/sqlplus -s archive/archive <<EOF
+	set heading off;
+	set pagesize 0;
+	set linesize 10240;
+	select SERIAL_NUMBER from STRUCTURE where info_sort_id=${i} ;
+	exit
+	EOF`
+	Result=`echo  "${Result}"|grep -v 已|grep -v selected`
+	Result=`echo  ${Result}|sed 's/ /,/g'`
+	ValueList="INFO_SORT_ID,STATUS,PAR_INFO_SORT_ID,TYPE,TITLE,${Result}"
+	#echo ${ValueList}
 
-/usr/lib/oracle/xe/app/oracle/product/10.2.0/server/bin/sqlplus -s archive/archive <<EOF
-${Create}
-exit
-EOF
+	#select name from info_sort t start with id= ${i} connect by id = prior parent_id;
 
+	Result=`/usr/lib/oracle/xe/app/oracle/product/10.2.0/server/bin/sqlplus -s archive/archive <<EOF
+	set heading off;
+	set pagesize 0;
+	set linesize 10240;
+	select name from info_sort where id=${i};
+	exit
+	EOF`
+	Result=`echo  "${Result}"|grep -v 已|grep -v selected`
+	ViewName=`echo ${Result}|awk '{for(i=0;i<=NF-1;i++)printf("%s ",$(NF-i));printf("\n");}'|sed 's/ /_/g'|sed 's/\_$//g'`
+	ViewName=T${i}_${ViewName}
 
-sleep 2
+	echo ViewName:${ViewName}
 
+	Create="Create Or Replace View ${ViewName} ( ${FieldList} ) As Select ${ValueList} From files where	INFO_SORT_ID=${i};"
+	echo debug
+	Create=`echo "${Create}"|sed 's/\//_/g'`
+	echo debug end
+	echo 
+	echo ${Create}
+	echo
+	echo "Executing ..."
+	echo
+
+	Result=`/usr/lib/oracle/xe/app/oracle/product/10.2.0/server/bin/sqlplus -s archive/archive <<EOF
+	${Create}
+	exit
+	EOF`
+	echo "${Result}"
+
+	echo "******************************************************************************"
+	sleep 1;
 done
-exit
-
-#echo  "${Result}"
-Result=`echo  "${Result}"|grep 911911911|grep -i -v INFO|awk '{print $2}'`
-echo ${Result}
-viewname=中文
-field1=分类编号
-field2=层次编码
-field3=上层分类编码
-field4=类型
-field5=标题
-field6=字段1
-field7=字段2
-field8=字段3
-
-rfield1=info_sort_id
-rfield2=status
-rfield3=par_info_sort_id
-rfield4=type
-rfield5=title
-rfield6=a1
-rfield7=a2
-rfield8=a3
-
-#/usr/lib/oracle/xe/app/oracle/product/10.2.0/server/bin/sqlplus -s archive/archive <<EOF 
-#Create Or Replace View $viewname ( ${field1},${field2},${field3},${field4},${field5},${field6},${field7},${field8} ) As Select a.${rfield1},a.${rfield2},a.${rfield3},a.${rfield4},a.${rfield5},a.${rfield6},a.${rfield7},a.${rfield8} From files a;
-#exit
-#EOF
