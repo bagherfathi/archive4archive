f55a4ede1f21f0d69c3596d02833e2c03b9141cb
tingwei.xiang <tingwei.xiang@c46cfd5c-866d-11de-9934-f1fc26e2a4cc>
2010-01-12 11:28:02 +0000

    update

---
+++ PopMenu/src/desktopapplication1/Static.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package desktopapplication1;
+
+/**
+ *
+ * @author Administrator
+ */
+class Static {
+
+}
