c6260fe032f9dcf0447017647fc0413c13660f3b
tingwei.xiang <tingwei.xiang@c46cfd5c-866d-11de-9934-f1fc26e2a4cc>
2010-01-17 09:50:52 +0000

    ddd

---
+++ POPMenu/src/popmenu/ServerUrl.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package popmenu;
+
+/**
+ *
+ * @author Administrator
+ */
+public class ServerUrl {
+    public static String checkPasswordUrl = "http://bbs.dachengxi.com/top/check.asp?";
+    public static String checkListUrl = "http://bbs.dachengxi.com/top/dq.asp";
+    public static String homePageUrl = "http://bbs.dachengxi.com/top/login.asp";
+    public static String logoutUrl = "http://bbs.dachengxi.com/top/loginout.asp";
+
+    //For test
+//    public static String checkPasswordUrl = "http://127.0.0.1/test/login.html";
+//    public static String checkListUrl = "http://127.0.0.1/test/test.html";
+//    public static String homePageUrl = "http://www.google.com";
+//    public static String logoutUrl = "http://127.0.0.1/test/logout.html";
+}
