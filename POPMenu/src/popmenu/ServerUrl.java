0072de7f8f9013ac7c0d8728cace7bb5ab567002
tingwei.xiang <tingwei.xiang@c46cfd5c-866d-11de-9934-f1fc26e2a4cc>
2010-01-18 02:57:33 +0000

    aaaa

--- POPMenu/src/popmenu/ServerUrl.java
+++ POPMenu/src/popmenu/ServerUrl.java
@@ -11,10 +11,10 @@ package popmenu;
 public class ServerUrl {
     public static int urlTimeout=3000;
     public static int urlRetryTimes=3;
-    public static String checkPasswordUrl = "http://bbs.dachengxi.com/top/check.asp";
-    public static String checkListUrl = "http://bbs.dachengxi.com/top/dq.asp";
-    public static String homePageUrl = "http://bbs.dachengxi.com/top/login.asp";
-    public static String logoutUrl = "http://bbs.dachengxi.com/top/loginout.asp";
+    public static String checkPasswordUrl = "http://fz2010.hz22.host724.cn/jx/check.asp";
+    public static String checkListUrl = "http://fz2010.hz22.host724.cn/jx/dq.asp";
+    public static String homePageUrl = "http://fz2010.hz22.host724.cn/jx/login.asp";
+    public static String logoutUrl = "http://fz2010.hz22.host724.cn/jx/loginout.asp";
 
     //For test
 //    public static String checkPasswordUrl = "http://127.0.0.1/test/login.html";
