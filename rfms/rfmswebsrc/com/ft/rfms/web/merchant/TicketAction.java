525aa9a4c23b0985bc3b4f34b2a6a5c5fd8ff309
luoxngood <luoxngood@c46cfd5c-866d-11de-9934-f1fc26e2a4cc>
2010-04-05 08:46:54 +0000

    up

--- rfms/rfmswebsrc/com/ft/rfms/web/merchant/TicketAction.java
+++ rfms/rfmswebsrc/com/ft/rfms/web/merchant/TicketAction.java
@@ -46,17 +46,18 @@ public class TicketAction extends BaseSimpleAction {
 		}
 		merchantService.saveOrUpdate(ticket);
 
-		// 生成飞券卡详细
-		for (int i = 0; i < ticket.getTicketCount(); i++) {
-			String seqNumber = merchantService.getTicketSysCode("ticket");
-			RfmsTicketDetail td = new RfmsTicketDetail();
-			td.setSeqNumber(seqNumber);// 生成下发卡编号
-			td.setMobile("");
-			td.setStatus(new Long(1));// 1.等待下发 2.已下发 3.已使用
-			td.setTicketId(ticket.getId());
-			merchantService.save(td);
+		if (ticket.getId() == null) {
+			// 生成飞券卡详细
+			for (int i = 0; i < ticket.getTicketCount(); i++) {
+				RfmsTicketDetail td = new RfmsTicketDetail();
+				String seqNumber = merchantService.getTicketSysCode("");
+				td.setSeqNumber(seqNumber);// 生成下发卡编号
+				td.setMobile("");
+				td.setStatus(new Long(1));// 1.等待下发 2.已下发 3.已使用
+				td.setTicketId(ticket.getId());
+				merchantService.save(td);
+			}
 		}
-
 		return unspecified(arg0, arg1, arg2, arg3);
 	}
 
---
+++ rfms/rfmswebsrc/com/ft/rfms/web/merchant/TicketDetailAction.java
@@ -0,0 +1,10 @@
+/**
+ * 
+ */
+package com.ft.rfms.web.merchant;
+
+import com.ft.singleTable.web.BaseSimpleAction;
+
+public class TicketDetailAction extends BaseSimpleAction {
+
+}
